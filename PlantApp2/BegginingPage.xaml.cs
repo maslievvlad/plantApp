294ec8e17178dec51ca819a8b9cc09390bc28208
maslievvlad <163740795+maslievvlad@users.noreply.github.com>
2024-04-02 08:34:53 +0300

    Add files via upload

---
+++ PlantApp2/BegginingPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PlantApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для BegginingPage.xaml
+    /// </summary>
+    public partial class BegginingPage : Page
+    {
+        public BegginingPage()
+        {
+            InitializeComponent();
+        }
+
+        private void But1_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new InputPage());
+        }
+    }
+}
